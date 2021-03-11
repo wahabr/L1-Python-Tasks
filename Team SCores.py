0094596c6b01bad8e06cb4e8f7b8146ca863346f
wahabr <wahabr@stu.otc.school.nz>
2021-03-12 12:56:51 +1300

    Created file and wrote code

---
+++ Team SCores.py
@@ -0,0 +1,11 @@
+match_1 = 3
+match_2 = 6
+match_3 = 10
+match_4 = 10
+match_5 = 6
+match_total = match_1 + match_2 + match_3 + match_4 + match_5
+print("The amount of points scored of game one was {}, in game two it was {}, in game three it as {}, in game four it was {}, and in game five the number of points scored was {}".format(match_1, match_2, match_3, match_4, match_5))
+print("The teams total game points = {}".format(match_total))
+
+
+
