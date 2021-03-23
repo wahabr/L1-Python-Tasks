622f0fcd9aeb5febe6b046180550219ff3f1630b
wahabr <wahabr@stu.otc.school.nz>
2021-03-23 15:00:42 +1300

    Created file and wrote code

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+number_1 = 1
+number_2 = 1
+for i in range (12):
+    while number_1 <= 12:
+        print("{} x {} = {}".format(number_1, number_2, number_1 * number_2))
+        number_1 += 1
+    number_1 = 1
+    number_2 += 1
+
