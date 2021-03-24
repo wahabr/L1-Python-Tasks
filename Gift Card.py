5b5fe0231a679cfd6da5f2e73b703265647ed314
wahabr <wahabr@stu.otc.school.nz>
2021-03-25 09:38:43 +1300

    added comments

--- Gift Card.py
+++ Gift Card.py
@@ -1,10 +1,14 @@
+#start amont
 gift_card = 50
+print("Opening Balance: ${}".format(gift_card))
+#costs
 initial_game = 35
 weapon_rapier = 2
 weapon_shortbow = 2
 weapon_dagger = 2
 armour_chainmail = 5
-print("Opening Balance: ${}".format(gift_card))
+#overall costs
 gift_card = gift_card - weapon_rapier - weapon_dagger - weapon_shortbow - armour_chainmail - initial_game
 print("Dungeons and Dragons: ${}, Rapier: ${}, Dagger: ${}, Shortbow: ${}, Chainmail: ${}".format(initial_game, weapon_rapier, weapon_dagger, weapon_shortbow, armour_chainmail))
+#closing balance
 print("Closing Balance: ${}".format(gift_card))
