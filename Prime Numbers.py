c63839189f37cbacef9eeba39f575773b358487f
wahabr <wahabr@stu.otc.school.nz>
2021-03-26 12:43:43 +1300

    improved code

--- Prime Numbers.py
+++ Prime Numbers.py
@@ -7,16 +7,15 @@ while a <= guess:
     b = 2
     while b <= guess:
         if guess == a * b:
-            b += 1
-        elif guess == 1:
-            b += 1
-        else:
             c = 2
-            b += 1
-if c == 1:
+        b += 1
+if guess == 1:
+    c = 2
+if c == 2:
     print("{} is not a prime number".format(guess))
 else:
     print("{} is a prime number".format(guess))
+
 
 
 
