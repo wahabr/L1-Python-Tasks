463505fd5a13078af4cd068e43fff3c23eafb774
wahabr <wahabr@stu.otc.school.nz>
2021-04-15 09:40:08 +1200

    updated code

--- Chocolate Bars.py
+++ Chocolate Bars.py
@@ -1,6 +1,7 @@
-num_people = 5
-num_pieces = 7
-chocolate_pieces = 18 * num_pieces
-print("You have {} pieces of chocolate to share evenly among {} people.".format(chocolate_pieces, num_people))
-print("In total each person will get {} bars of chocolate, {} pieces, and there will be {} pieces left over".format(chocolate_pieces // num_people // num_pieces, chocolate_pieces // num_people, chocolate_pieces % num_people))
-
+try:
+    num_people = int(input("How many people?"))
+    num_bars = int(input("How many bars of chocolate are there?"))
+    num_pieces = int(input("How many pieces are in each bar?"))
+    chocolate_pieces = num_bars * num_pieces
+    print("You have {} pieces of chocolate to share evenly among {} people.".format(chocolate_pieces, num_people))
+    print("In total each person will get {} whole bars of chocolate, {} extra pieces, and there will be {} pieces left over".format(chocolate_pieces // num_people // num_pieces, chocolate_pieces // num_people % num_pieces, chocolate_pieces % num_people))
