ce21c761c868f9f5a133baf97fd39c7c9eb811cf
wahabr <wahabr@stu.otc.school.nz>
2021-04-16 12:46:56 +1200

    updated code

--- Chocolate Bars.py
+++ Chocolate Bars.py
@@ -1,7 +1,13 @@
 try:
-    num_people = int(input("How many people?"))
+    num_people = int(input("How many people are there?"))
     num_bars = int(input("How many bars of chocolate are there?"))
     num_pieces = int(input("How many pieces are in each bar?"))
     chocolate_pieces = num_bars * num_pieces
-    print("You have {} pieces of chocolate to share evenly among {} people.".format(chocolate_pieces, num_people))
-    print("In total each person will get {} whole bars of chocolate, {} extra pieces, and there will be {} pieces left over".format(chocolate_pieces // num_people // num_pieces, chocolate_pieces // num_people % num_pieces, chocolate_pieces % num_people))
+    if num_people > 0 and num_bars > 0 and num_pieces > 0:
+        print("You have {} pieces of chocolate to share evenly among {} people.".format(chocolate_pieces, num_people))
+        print("In total each person will get {} whole bars of chocolate, {} extra pieces, and there will be {} pieces left over".format(chocolate_pieces // num_people // num_pieces, chocolate_pieces // num_people % num_pieces, chocolate_pieces % num_people))
+    else:
+        print("You cannot have negative friends or negative chocolate!")
+except ValueError:
+    print("Please input a number greater than 0.")
+
